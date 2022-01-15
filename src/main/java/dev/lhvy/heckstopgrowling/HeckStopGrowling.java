b9b575eda2dd233459ae95e40f6bb7d6693249c3
Lucas <me@lhvy.dev>
2022-01-15 22:55:30 +1100

    Add bStats

--- src/main/java/dev/lhvy/heckstopgrowling/HeckStopGrowling.java
+++ src/main/java/dev/lhvy/heckstopgrowling/HeckStopGrowling.java
@@ -1,10 +1,14 @@
 package dev.lhvy.heckstopgrowling;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeckStopGrowling extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new HeckListener(), this);
+
+        @SuppressWarnings("unused")
+        Metrics metrics = new Metrics(this, 13960);
     }
 }
